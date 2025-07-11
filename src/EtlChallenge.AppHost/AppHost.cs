40b2e688984529254a717de472ee091f94aa0866
juanochando <57346655+juanochando@users.noreply.github.com>
2025-07-11 12:49:43 +0000

    Added basic projects for UI and Services

--- src/EtlChallenge.AppHost/AppHost.cs
+++ src/EtlChallenge.AppHost/AppHost.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.EltChallenge_UI>("ui");
+builder.AddProject<Projects.EtlChallenge_LoadService>("ui");
+builder.AddProject<Projects.EtlChallenge_UnzipService>("ui");
+builder.AddProject<Projects.EtlChallenge_ValidateService>("ui");
+
 builder.Build().Run();
